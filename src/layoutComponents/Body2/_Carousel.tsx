e1bf257d035a7cde75f8cc741d01eb0073fb0c7f
noellimx <67155581+noellimx@users.noreply.github.com>
2025-02-28 22:00:46 +0800

    +body2 step 1 of carousel transition

--- src/layoutComponents/Body2/_Carousel.tsx
+++ src/layoutComponents/Body2/_Carousel.tsx
@@ -32,6 +32,7 @@ const CircularCarousel: React.FC = () => {
         // Cleanup the observer on unmount
         return () => {
             if (carouselWrapperRef.current) {
+                // eslint-disable-next-line react-hooks/exhaustive-deps
                 observer.unobserve(carouselWrapperRef.current);
             }
         };
