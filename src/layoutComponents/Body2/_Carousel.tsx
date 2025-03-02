2a36b0a97c3416a034f7d0cbbc83e5afb742d4ce
noellimx <67155581+noellimx@users.noreply.github.com>
2025-03-02 21:13:07 +0800

    + body2 carousel, laser point to exhaust for tablet

--- src/layoutComponents/Body2/_Carousel.tsx
+++ src/layoutComponents/Body2/_Carousel.tsx
@@ -55,7 +55,7 @@ const _circleLaserOffsets: circleOffsetsT = {
     },
     [Device.tablet]: {
         "bonnet": {x: "260px", y: "260px"},
-        "exhaust": {x: "0px", y: "0px"},
+        "exhaust": {x: "120px", y: "290px"},
         "wheel": {x: "0px", y: "0px"},
         "side_profile": {x: "0px", y: "0px"},
     },
