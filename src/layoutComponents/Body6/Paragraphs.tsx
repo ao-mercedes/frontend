876a6394e18e6d0c9e8789bd38500ab375fa5017
noellimx <67155581+noellimx@users.noreply.github.com>
2025-03-05 07:32:40 +0800

    add bodyheader component and prepare body6

---
+++ src/layoutComponents/Body6/Paragraphs.tsx
@@ -0,0 +1,53 @@
+import {COLORS} from "../../utils/constants/constants.ts";
+
+import {Typography} from "antd";
+
+export const Paragraph: React.FC<{ paragraphLineHeight: string, text: string, fontSize: string }> = ({
+                                                                                                         text,
+                                                                                                         fontSize,
+                                                                                                         paragraphLineHeight
+                                                                                                     }) => {
+    return <div className="ao-body5-content-paragraph"
+                style={{
+                    display: "flex",
+                    height: "max-content",
+                    color: "white",
+                }}>
+        <Typography.Text style={{color: COLORS.PURE_WHITE, fontSize: fontSize, lineHeight: paragraphLineHeight}}>
+            {text}
+        </Typography.Text>
+    </div>;
+};
+
+export const Paragraphs: React.FC<{
+    fontSize: string,
+    paragraphLineHeight: string,
+    rowGap: string,
+    texts: string[]
+}> = ({
+          fontSize,
+          paragraphLineHeight,
+          texts,
+          rowGap
+      }) => {
+    return <div className="ao-body5-content-paragraphs" style={{
+        display: "flex",
+        height: "max-content",
+        fontSize: "30px",
+        flexDirection: "column",
+        justifyContent: "center",
+        paddingTop: "20px",
+        rowGap: rowGap,
+    }}>
+        {texts.map((text, index) => {
+            return <div key={index}>
+                <Paragraph
+                    fontSize={fontSize}
+                    paragraphLineHeight={paragraphLineHeight}
+                    text={text}></Paragraph>
+            </div>;
+        })}
+    </div>;
+};
+
+export default Paragraphs;
---
+++ src/layoutComponents/Body6/index.tsx
@@ -0,0 +1,20 @@
+import "./index.css";
+
+import {Device} from "../../utils/constants/constants.ts";
+
+
+export const Body6: React.FC<{ device: Device, }> = () => {
+    return <div className="ao-body6"
+                style={{
+                    display: "flex",
+                    position: "relative",
+                    flexDirection: "column",
+                    width: "100%",
+                    overflow: "auto",
+                    height: "min-content",
+                }}>
+        aaass
+    </div>;
+};
+
+export default Body6;
